24a066bdb47df183c47d581a3e078d5df19a4328
Babu Izhan <babu.izhan@shopup.org>
2024-02-06 13:12:33 +0530

    use local config

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -3,10 +3,10 @@ export const config = {
     currentShotsPath: "./cypress/screenshots",
   },
   // OSS mode 
-  // generateOnly: true,
-  // failOnDifference: true
+  generateOnly: true,
+  failOnDifference: true
   
   // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
-  lostPixelProjectId: "cls7guw1prxpwhf0e79z2fe5k",
-  apiKey: process.env.LOST_PIXEL_API_KEY,
+  // lostPixelProjectId: "cls7guw1prxpwhf0e79z2fe5k",
+  // apiKey: process.env.LOST_PIXEL_API_KEY,
 };
